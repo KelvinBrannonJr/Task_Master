305fcc7e739e87117b68e4102a513aea315aaa3c
Kelvin <Kelzjr56@gmail.com>
2023-05-06 21:56:34 -0400

    Optimized code using with Context

--- task_master.py
+++ task_master.py
@@ -9,41 +9,60 @@ while isLooping:
             task = input("Enter a Task: ") + "\n"
             format_task = task.title()
 
-            file = open("tasks.txt", 'r')
-            tasks = file.readlines()
-            file.close()
+            # With context manager - same as file.open() but, it automatically closes file after context scope
+            with open("tasks.txt", 'r') as file:
+                tasks = file.readlines()
 
             tasks.append(format_task)
 
-            file = open('tasks.txt', 'w')
-            file.writelines(tasks)
-            file.close()
-
-            print(format_task)
+            with open("tasks.txt", 'w') as file:
+                file.writelines(tasks)
 
         case 'Show':
-            file = open("tasks.txt", 'r')
-            tasks = file.readlines()
-            file.close()
+            with open("tasks.txt", 'r') as file:
+                tasks = file.readlines()
 
-            for index, format_task in enumerate(tasks):
-                row = f"{index + 1} - {format_task}"
+            # List Comprehension - inline for-loop returns a list without declaring empty list for new items.
+            new_tasks = [item.strip('\n') for item in tasks]
+
+            # Same way but with a for-loop ** Longer route but same as list comprehension
+            # new_tasks = []
+            # for item in tasks:
+            #     item = item.strip('\n')
+            #     new_tasks.append(item)
+            #
+            for index, item in enumerate(new_tasks):
+                row = f"{index + 1} - {item}"
                 print(row)
 
         case 'Edit':
-            print(task_array)
-            edit_choice = int(input("Starting with 1, which item number in the collection do you want to edit? "))
-            refactored_index = edit_choice - 1
+            edit_number = int(input("Starting with 1, which item number in the collection do you want to edit? "))
+            edit_number = edit_number - 1
 
-            new_task = input("What would you like to change the task to? ")
-            print(new_task)
+            with open("tasks.txt", 'r') as file:
+                tasks = file.readlines()
 
-            task_array[refactored_index] = new_task.title().strip()
-            print("Ok your changes have been made.", task_array)
+            new_task = input("What would you like to change this to? ")
+            tasks[edit_number] = new_task.title() + '\n'
+
+            with open("tasks.txt", 'w') as file:
+                file.writelines(tasks)
 
         case 'Complete':
             complete = int(input("Enter a number of task you have completed: \n"))
-            task_array.pop(complete - 1)
+
+            with open("tasks.txt", 'r') as file:
+                tasks = file.readlines()
+
+            selected_index = complete - 1
+            completed_task = tasks[selected_index].strip('\n')
+            tasks.pop(selected_index)
+
+            with open("tasks.txt", 'w') as file:
+                file.writelines(tasks)
+
+            message = f"Task: {completed_task}, was completed and removed"
+            print(message)
 
         case 'Exit':
             isLooping = False
@@ -53,6 +72,3 @@ while isLooping:
             print("Sorry that is not a valid choice...")
 
 print("Exiting program, goodbye..")
-
-
-
