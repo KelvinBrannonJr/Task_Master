829a71c3126fd2d4037cf87f42370563c8e618e1
Kelvin <Kelzjr56@gmail.com>
2023-05-12 17:54:23 -0400

    Optimized code by adding some functions for redundant READ code

--- task_master.py
+++ task_master.py
@@ -1,5 +1,13 @@
 isLooping = True
 
+
+def get_tasks():
+    # With context manager - same as file.open() but, it automatically closes file after context scope
+    with open("tasks.txt", 'r') as local_file:
+        local_tasks = file.readlines()
+    return local_tasks
+
+
 while isLooping:
     user_choice = input("For a task, would you like to 'Add', 'Show', 'Edit', 'Complete', or 'Exit'?: ").title()
     format_choice = user_choice.strip()
@@ -8,9 +16,7 @@ while isLooping:
         task = input("Enter a Task: ").title()
         format_task = task.strip()
 
-        # With context manager - same as file.open() but, it automatically closes file after context scope
-        with open("tasks.txt", 'r') as file:
-            tasks = file.readlines()
+        tasks = get_tasks()
 
         tasks.append(format_task + "\n")
 
@@ -18,8 +24,7 @@ while isLooping:
             file.writelines(tasks)
 
     elif 'Show' in user_choice:
-        with open("tasks.txt", 'r') as file:
-            tasks = file.readlines()
+        tasks = get_tasks()
 
         # List Comprehension - inline for-loop returns a list without declaring empty list for new items.
         new_tasks = [item.strip('\n') for item in tasks]
@@ -39,8 +44,7 @@ while isLooping:
             edit_number = int(input("Starting with 1, which item number in the collection do you want to edit? "))
             edit_number = edit_number - 1
 
-            with open("tasks.txt", 'r') as file:
-                tasks = file.readlines()
+            tasks = get_tasks()
 
             new_task = input("What would you like to change this to? ")
             tasks[edit_number] = new_task.title() + '\n'
@@ -50,14 +54,13 @@ while isLooping:
 
         except ValueError:
             print("Invalid selection of item..Use only the number next to task item")
-            continue    # continue - will rerun the while loop back at the top, nothing below will execute
+            continue  # continue - will rerun the while loop back at the top, nothing below will execute
 
     elif 'Complete' in user_choice:
         try:
             complete = int(input("Enter a number of task you have completed: \n"))
 
-            with open("tasks.txt", 'r') as file:
-                tasks = file.readlines()
+            tasks = get_tasks()
 
             selected_index = complete - 1
             completed_task = tasks[selected_index].strip('\n')
