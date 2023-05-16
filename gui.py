e188cf3e92f09f2dfca10cde13050eb3f8e4e811
Kelvin <Kelzjr56@gmail.com>
2023-05-16 09:43:27 -0400

    Increased GUI Size and added font, implemented Add btn functionality

--- gui.py
+++ gui.py
@@ -2,10 +2,28 @@ import functions
 import PySimpleGUI as gui
 
 label = gui.Text("Type in a Task: ")
-input_box = gui.InputText(tooltip="Enter Task")
+input_box = gui.InputText(tooltip="Enter Task", key='task')
 add_button = gui.Button("Add")
 
-window = gui.Window('Task Master', layout=[[label, input_box, add_button]])
-window.read()
+window = gui.Window('Task Master',
+                    layout=[
+                            [label, input_box, add_button]
+                    ],
+                    font=('Helvetica', 15))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            tasks = functions.get_tasks()
+            new_task = values['task'] + "\n"
+            tasks.append(new_task)
+            functions.write_tasks(tasks)
+
+        case gui.WIN_CLOSED:
+            break
+
 window.close()
 
