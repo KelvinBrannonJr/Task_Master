c8a95b08470c1aa777110a6f36d891ad6860cd4b
Kelvin <Kelzjr56@gmail.com>
2023-05-17 11:39:32 -0400

    Implemented theme and live time in GUI, added final touches to complete application! ALL DONE

--- cli.py
+++ cli.py
@@ -1,16 +1,17 @@
-# from functions import get_tasks, write_tasks # *** Same as below but have to write functions out one by one.
 import functions
 import time
 
-isLooping = True
 
+# Time format
 current_time = time.strftime("%b %d %Y %H:%M:%S")
 print("Currently it is", current_time)
 
-while isLooping:
+# Event loop for user input
+while True:
     user_choice = input("For a task, would you like to 'Add', 'Show', 'Edit', 'Complete', or 'Exit'?: ").title()
     format_choice = user_choice.strip()
 
+    # Add - to add a task to list
     if 'Add' in user_choice:
         task = input("Enter a Task: ").title()
         format_task = task.strip()
@@ -21,17 +22,20 @@ while isLooping:
 
         functions.write_tasks(tasks)
 
+    # Show - to show tasks in the current list
     elif 'Show' in user_choice:
         tasks = functions.get_tasks()
 
-        # List Comprehension - inline for-loop returns a list without declaring empty list for new items.
+        # List Comprehension to task items and return it in a new collection
         new_tasks = [item.strip('\n') for item in tasks]
 
         for index, item in enumerate(new_tasks):
             row = f"{index + 1} - {item}"
             print(row)
 
+    # Edit - get a task in the list at it's index and overwrite the value
     elif 'Edit' in user_choice:
+        # try catch exception block
         try:
             edit_number = int(input("Starting with 1, which item number in the collection do you want to edit? "))
             edit_number = edit_number - 1
@@ -43,10 +47,12 @@ while isLooping:
 
             functions.write_tasks(tasks)
 
+        # continue will rerun the while loop back at the top, nothing below will execute
         except ValueError:
             print("Invalid selection of item..Use only the number next to task item")
-            continue  # continue - will rerun the while loop back at the top, nothing below will execute
+            continue
 
+    # Complete and remove task from the list
     elif 'Complete' in user_choice:
         try:
             complete = int(input("Enter a number of task you have completed: \n"))
@@ -65,10 +71,11 @@ while isLooping:
             print("That item number does not exist..")
             continue
 
+    # User enters Exit to terminate the program
     elif 'Exit' in user_choice:
-        isLooping = False
         break
 
+    # Else to handle invalid entry
     else:
         print("Sorry that is not a valid choice...")
 
--- gui.py
+++ gui.py
@@ -1,28 +1,36 @@
 import functions
 import PySimpleGUI as gui
+import time
+
+# Design Theme
+gui.theme("DarkBlue14")
+
+# Date and Time element
+clock = gui.Text("", key='clock')
 
 # ADD GUI elements
-label = gui.Text("Type in a Task: ")
-input_box = gui.InputText(tooltip="Enter Task", key='task')
+label_add = gui.Text("Type in a Task: ")
+input_box_add = gui.InputText(tooltip="Enter Task", key='task')
 add_button = gui.Button("Add")
 
 # Edit GUI elements
-label_tasks = gui.Text("Your Tasks:")
+label_edit = gui.Text("Your Tasks:")
 list_box = gui.Listbox(values=functions.get_tasks(), key='tasks', enable_events=True, size=(45, 10))
 edit_button = gui.Button("Edit")
 
-# Complete GUI elements
+# Complete GUI element
 complete_button = gui.Button("Complete")
 
-# Exit GUI elements
+# Exit GUI element
 exit_button = gui.Button("Exit")
 
 
 # GUI window layout
 window = gui.Window('Task Master',
                     layout=[
-                            [label, input_box, add_button],
-                            [label_tasks],
+                            [clock],
+                            [label_add, input_box_add, add_button],
+                            [label_edit],
                             [list_box, edit_button, complete_button],
                             [exit_button]
                     ],
@@ -30,10 +38,9 @@ window = gui.Window('Task Master',
 
 # Connecting GUI elements with backend Functionality
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['tasks'])
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d %Y %H:%M:%S"))
+
     match event:
         case "Add":
             tasks = functions.get_tasks()
@@ -41,24 +48,33 @@ while True:
             tasks.append(new_task)
             functions.write_tasks(tasks)
             window['tasks'].update(values=tasks)
+            window['task'].update(value="")
 
         case "Edit":
-            task_to_edit = values['tasks'][0]
-            new_task = values['task']
+            try:
+                task_to_edit = values['tasks'][0]
+                new_task = values['task']
 
-            tasks = functions.get_tasks()
-            index = tasks.index(task_to_edit)
-            tasks[index] = new_task
-            functions.write_tasks(tasks)
-            window['tasks'].update(values=tasks)
+                tasks = functions.get_tasks()
+                index = tasks.index(task_to_edit)
+                tasks[index] = new_task
+                functions.write_tasks(tasks)
+                window['tasks'].update(values=tasks)
+                window['task'].update(value="")
+
+            except IndexError:
+                gui.popup("Please select an item first.", font=("Helvetica", 20))
 
         case "Complete":
-            task_to_complete = values['tasks'][0]
-            tasks = functions.get_tasks()
-            tasks.remove(task_to_complete)
-            functions.write_tasks(tasks)
-            window['tasks'].update(values=tasks)
-            window['task'].update(value="")
+            try:
+                task_to_complete = values['tasks'][0]
+                tasks = functions.get_tasks()
+                tasks.remove(task_to_complete)
+                functions.write_tasks(tasks)
+                window['tasks'].update(values=tasks)
+                window['task'].update(value="")
+            except IndexError:
+                gui.popup("Please select an item first.", font=("Helvetica", 20))
 
         case "Exit":
             break
