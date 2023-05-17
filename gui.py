ad9c86cdb05323ac396eb45f74edc11e1f44028a
Kelvin <Kelzjr56@gmail.com>
2023-05-17 10:37:05 -0400

    Implemented Complete and Exit buttons, with back-end functionality

--- gui.py
+++ gui.py
@@ -11,12 +11,20 @@ label_tasks = gui.Text("Your Tasks:")
 list_box = gui.Listbox(values=functions.get_tasks(), key='tasks', enable_events=True, size=(45, 10))
 edit_button = gui.Button("Edit")
 
+# Complete GUI elements
+complete_button = gui.Button("Complete")
+
+# Exit GUI elements
+exit_button = gui.Button("Exit")
+
+
 # GUI window layout
 window = gui.Window('Task Master',
                     layout=[
                             [label, input_box, add_button],
                             [label_tasks],
-                            [list_box, edit_button]
+                            [list_box, edit_button, complete_button],
+                            [exit_button]
                     ],
                     font=('Helvetica', 15))
 
@@ -44,11 +52,23 @@ while True:
             functions.write_tasks(tasks)
             window['tasks'].update(values=tasks)
 
+        case "Complete":
+            task_to_complete = values['tasks'][0]
+            tasks = functions.get_tasks()
+            tasks.remove(task_to_complete)
+            functions.write_tasks(tasks)
+            window['tasks'].update(values=tasks)
+            window['task'].update(value="")
+
+        case "Exit":
+            break
+
         case "tasks":
             window['task'].update(value=values['tasks'][0])
 
         case gui.WIN_CLOSED:
             break
+
 
 window.close()
 
