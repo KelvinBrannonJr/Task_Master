a256e26a21e8e20cd669b352ab328cb1546945fc
Kelvin <Kelzjr56@gmail.com>
2023-05-18 12:35:18 -0400

    Web App -Added feature to add task to list

--- web.py
+++ web.py
@@ -1,19 +1,35 @@
 import streamlit as st
 import functions
 
-tasks = functions.get_tasks(functions.FILEPATH)
-new_task = functions.write_tasks(functions.FILEPATH)
+# Get list of tasks
+tasks = functions.get_tasks()
 
 
+# Add new task to list
+def add_task():
+    new_task = st.session_state['-NEW-TASK-'] + "\n"
+    tasks.append(new_task)
+    functions.write_tasks(tasks)
+
+
+# Layout of items in app,
+    # The order on the page correlates with the order they listed in this file
 st.title("Task Master Web App")
 st.subheader("Maximize your productivity")
 st.write("Please select from the menu")
 
 
+# Iterate through the list and create a checkbox for each item
 for task in tasks:
     st.checkbox(task)
 
 
-st.text_input(label="", placeholder="Enter a new task")
-
+# Input box to add new task item
+st.text_input(
+    label="Add Task",
+    label_visibility="collapsed",
+    placeholder="Enter a new task",
+    on_change=add_task,
+    key='-NEW-TASK-'
+)
 
